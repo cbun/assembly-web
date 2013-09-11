3a62e713eb84f63f58f39abe1fe389dd7f6776d0
Chris Bun <chrisbun@gmail.com>
2013-09-10 23:06:43 -0500

    * Style tweaks * Responsive show/hiding * Proper extraction of Shock message * Shock list files * Separate partial views

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -14,6 +14,20 @@ app.config(function ($routeProvider) {
       });
   });
 
+app.config(['$httpProvider', function($httpProvider) {
+        delete $httpProvider.defaults.headers.common["X-Requested-With"]
+      }]);
+
 app.config(function(RestangularProvider){
   RestangularProvider.setBaseUrl("http://140.221.84.203:8000/");
+
+
+  RestangularProvider.setResponseExtractor(function(response, operation, what, url) {
+        // Extract data objects from shock node
+        if (what === "node") {
+            var newResponse = response.data;
+            return newResponse;
+        }
+        return response;
+    });
 });
